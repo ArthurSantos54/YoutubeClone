7101707fc8f5278a84bb2c85a81dae2fb174f4d2
Antonio Gomes <60694694+ac-gomes@users.noreply.github.com>
2021-03-06 14:52:26 -0300

    função para pegar todos os dados de uma vez

--- RefProjeto/form.js
+++ RefProjeto/form.js
@@ -1,20 +1,19 @@
-
 let form1 = document.getElementById('form-cadastro');
+
+//Carregar os dados na tabela
+let json = JSON.parse(localStorage.getItem('userData')) || [];
 
 function getDataForm () {
 
   let itemInput = [];
-  let json ='';
 
   [...form1].forEach(el => {
 
-      if(el.id != 'enviar' && el.id != 'trabalha-checkbox'){
-
+      if(el.id != 'enviar'){
         itemInput.push(`${el.id}: ${el.value}`);
-
       }
 
-      json = JSON.stringify(itemInput);
+      json = itemInput;
 
   });
 
@@ -24,6 +23,6 @@ function getDataForm () {
 
 function saveData(data){
 
-  localStorage.setItem('userData', data)
+  localStorage.setItem('userData', JSON.stringify(data))
 
 }
