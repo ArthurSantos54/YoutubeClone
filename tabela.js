9f2b7daf10352daa22fc601057965f8081ea4096
ArthurSantos54 <Arthursantos4367@gmail.com>
2021-03-20 16:25:00 -0300

    salvar

---
+++ tabela.js
@@ -0,0 +1,36 @@
+//  <tr>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+
+//             </tr>
+//             <tr>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//             </tr>
+//             <tr>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//                 <td> Exemplo </td>
+//             </tr>
+
+            let json =  JSON.parse(localStorage.getItem("userData")) || [];
+
+            let tabela = document.getElementById("dados-usuario")
+            console.log(json);
