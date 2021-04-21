199e9eca03f0252f527c79c84e9cb82a7edda0d1
Antonio Gomes <60694694+ac-gomes@users.noreply.github.com>
2021-04-21 18:07:51 -0300

    dica-js-infor-card

---
+++ Card/card.js
@@ -0,0 +1,8 @@
+let nomeUsuarioEl = document.getElementById('nome-perfil');
+let bioEl = document.getElementById('bio-perfil');
+
+nomeUsuarioEl.innerHTML = "João"
+console.log(bioEl.innerText) // olha lá no colose do navegador
+
+//como exemplo acima você vai pegar os elento que vão receber as informaçãos
+// e colonar no cartão
