cef83ca9af120a6b4ff6eff5961cc56b10f686f2
ArthurSantos54 <Arthursantos4367@gmail.com>
2021-04-24 12:03:24 -0300

    Alterei os inputs

--- Card/card.js
+++ Card/card.js
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
 function mudarTitulo(novoTitulo) {
     document.querySelector("#titulo").innerHTML = nome.value;
     document.querySelector("#profissao").innerHTML = Profissão.value;
-   
+    document.querySelector("#bio-perfil").innerHTML = biografia.value;
 }
 
 
-=======
-let nomeUsuarioEl = document.getElementById('nome-perfil');
+
+let nomeUsarioEL = document.getElementById('nome-perfil');
 let bioEl = document.getElementById('bio-perfil');
 
-nomeUsuarioEl.innerHTML = "João"
-console.log(bioEl.innerText) // olha lá no colose do navegador
 
-//como exemplo acima você vai pegar os elento que vão receber as informaçãos
+nomeUsuarioEl.innerHTML = "joão"
+console.log(bioEL.innerText) //olha lá no console do navegador
+
+//como exemplo acima você vai pegar os elementos que vão receber as informações
 // e colonar no cartão
->>>>>>> 0c20894ba313f19aefba77c08887f5b512502658
