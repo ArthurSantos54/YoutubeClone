4173be5fca267cc42a8497ea12f67b9e5d2e6ff9
ArthurSantos54 <Arthursantos4367@gmail.com>
2021-05-01 18:12:58 -0300

    .

--- Card/card.js
+++ Card/card.js
@@ -2,12 +2,16 @@
 var inputNome = document.getElementById("nome");
 var inputProfissao = document.getElementById("Profissão");
 var inputBiografia = document.getElementById("biografia");
+var inputImagem = document.getElementById("Imagem");
 
 var nomeTitulo = document.getElementById("titulo");
 var profissao = document.getElementById("profissao");
 var bioPerfil = document.getElementById("bio-perfil");
 
 var botaoEl = document.querySelector('.nbtn-enviar')
+var imgEl = document.getElementById('img-perfil')
+var imagemPadrao = "https://static8.depositphotos.com/1003153/893/v/600/depositphotos_8938809-stock-illustration-example-rubber-stamp.jpg"
+
 
 function inserirValores(
     nomeInput,
@@ -18,18 +22,30 @@ function inserirValores(
     perfilBio
     ) {
     if (nomeInput.value != "" && profissaoInput.value != "" && biografiaInput.value != ""){
+
         tituloNome.innerHTML = nomeInput.value;
         profissao.innerHTML = profissaoInput.value;
         perfilBio.innerHTML = biografiaInput.value;
+
+        document.addEventListener("DOMContentLoaded", function(){
+            imgEl.src = novaImagem.value;
+         });
+        
     }else{
         alert("Por favor preencher todos campos!")
     }
 }
 
 
+document.addEventListener("DOMContentLoaded", function(){
+    imgEl.src = imagemPadrao;
+ });
+
+
 
 botaoEl.addEventListener('click', function (){
     inserirValores(inputNome,inputProfissao,inputBiografia,nomeTitulo,profissao,bioPerfil);
+    
 });
 
 botaoEl.addEventListener('keypress', function (e){
@@ -40,4 +56,17 @@ botaoEl.addEventListener('keypress', function (e){
     }
 })
 
-    
+// const pegarImagem = (imagemAtual, novaImagem) => {      
+//     document.addEventListener("DOMContentLoaded", _ => {            
+//     if(novaImagem != ""){        
+//         imgEl.src = novaImagem.value;
+//         console.log(imagemAtual)  
+//     }else{        
+//         imgEl.src = imagemAtual;  
+//     } 
+//  }); 
+// }
+
+
+
+
