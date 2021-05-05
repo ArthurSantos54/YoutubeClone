7bd45d06c36e2d9838c4ef0a2bbadf1b03247cf9
Antonio Gomes <60694694+ac-gomes@users.noreply.github.com>
2021-05-04 23:13:35 -0300

    Correção troca da imagem

--- Card/card.js
+++ Card/card.js
@@ -7,10 +7,7 @@ var nomeTitulo = document.getElementById("titulo");
 var profissao = document.getElementById("profissao");
 var bioPerfil = document.getElementById("bio-perfil");
 
-var botaoEl = document.querySelector('.nbtn-enviar')
-var imgEl = document.getElementById('img-perfil')
-var imagemPadrao = "https://static8.depositphotos.com/1003153/893/v/600/depositphotos_8938809-stock-illustration-example-rubber-stamp.jpg"
-
+var botaoEl = document.querySelector('.nbtn-enviar');
 
 function inserirValores(
     nomeInput,
@@ -21,51 +18,43 @@ function inserirValores(
     perfilBio
     ) {
     if (nomeInput.value != "" && profissaoInput.value != "" && biografiaInput.value != ""){
-
         tituloNome.innerHTML = nomeInput.value;
         profissao.innerHTML = profissaoInput.value;
         perfilBio.innerHTML = biografiaInput.value;
-
-        document.addEventListener("DOMContentLoaded", function(){
-            imgEl.src = novaImagem.value;
-         });
-        
+        pegarNovaImagem (inputImagem);
     }else{
         alert("Por favor preencher todos campos!")
     }
 }
 
-
-document.addEventListener("DOMContentLoaded", function(){
-    imgEl.src = imagemPadrao;
- });
-
-
-
 botaoEl.addEventListener('click', function (){
-    inserirValores(inputNome,inputProfissao,inputBiografia,nomeTitulo,profissao,bioPerfil);
-    
+    inserirValores(
+        inputNome,
+        inputProfissao,
+        inputBiografia,
+        nomeTitulo,
+        profissao,
+        bioPerfil
+    );
 });
 
 botaoEl.addEventListener('keypress', function (e){
     if(e.key == "Enter"){
-        inserirValores(inputNome,inputProfissao,inputBiografia,nomeTitulo,profissao,bioPerfil);
+        inserirValores(
+            inputNome,
+            inputProfissao,
+            inputBiografia,
+            nomeTitulo,
+            profissao,
+            bioPerfil
+        );
     }else{
-      
+
     }
 })
 
-// const pegarImagem = (imagemAtual, novaImagem) => {      
-//     document.addEventListener("DOMContentLoaded", _ => {            
-//     if(novaImagem != ""){        
-//         imgEl.src = novaImagem.value;
-//         console.log(imagemAtual)  
-//     }else{        
-//         imgEl.src = imagemAtual;  
-//     } 
-//  }); 
-// }
-
-
-
-
+function pegarNovaImagem (novaImagem){
+    if(novaImagem.value != ""){
+        document.getElementById('img-perfil').src = novaImagem.value;
+    };
+}
